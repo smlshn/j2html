e42d59083ebbc4426f62c3f5462d9b5188c6235a
smlshn <ismailsahinvan65@gmail.com>
2019-06-12 14:36:42 +0300

    fix import error

--- src/main/java/j2html/attributes/Attribute.java
+++ src/main/java/j2html/attributes/Attribute.java
@@ -3,6 +3,7 @@ package j2html.attributes;
 import j2html.Config;
 import j2html.tags.Renderable;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Attribute implements Renderable {
     private String name;
